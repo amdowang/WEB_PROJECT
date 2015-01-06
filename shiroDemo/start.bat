8096137b4a05317682c9ba538a82d136259e36e4
Administrator <Administrator@2011-20130101VX>
2015-01-06 23:26:12 +0800

    d

--- shiroDemo/start.bat
+++ shiroDemo/start.bat
@@ -5,9 +5,9 @@ echo [Pre-Requirement] Makesure install Maven 3.0.3+ and set the PATH.
 set MVN=mvn
 set MAVEN_OPTS=%MAVEN_OPTS% -XX:MaxPermSize=128m
 
+echo [Step 1] Install all springside modules and archetype to local maven repository.
 
-call %MVN% jetty:run 
-if errorlevel 1 goto error
+call %MVN% jetty:run
 
 echo [INFO] Please wait a moment. When you see "[INFO] Started Jetty Server" in both 1 popup consoles, you can access below demo sites:
 echo [INFO] http://localhost:8080/shiroDemo
