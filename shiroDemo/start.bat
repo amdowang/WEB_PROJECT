719809ab6d8e5a4487bce260542166ca55576d96
Administrator <Administrator@WWW-B24FC558DB1>
2015-01-07 10:12:45 +0800

    update commnand

--- shiroDemo/start.bat
+++ shiroDemo/start.bat
@@ -5,7 +5,9 @@ echo [Pre-Requirement] Makesure install Maven 3.0.3+ and set the PATH.
 set MVN=mvn
 set MAVEN_OPTS=%MAVEN_OPTS% -XX:MaxPermSize=128m
 
-call %MVN% jetty:run
+start "shiroDemo" %MVN% jetty:run
+
+if errorlevel 1 goto error
 
 echo [INFO] Please wait a moment. When you see "[INFO] Started Jetty Server" in both 1 popup consoles, you can access below demo sites:
 echo [INFO] http://localhost:8080/shiroDemo
